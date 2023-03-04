5e07ecd9b84d829888cf62067c31c6607bc8c2c8
javi0098 <javi0098@gmail.com>
2023-03-04 12:37:42 -0300

    ADD examples

---
+++ validate.js
@@ -0,0 +1,9 @@
+function validar() {
+   var entero =   document.getElementById("selector").value;
+   if (entero == 1){(document.getElementById("label1")).innerHTML =  "VALOR = $$500pe"}
+   if (entero == 2){(document.getElementById("label1")).innerHTML =  "VALOR = $$1000pe"}
+   if (entero == 3){(document.getElementById("label1")).innerHTML =  "VALOR = $$1500pe"}
+
+
+   
+  }
